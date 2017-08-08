ef8809ce0552dc79a536223e946c402eee5a4d89
Will <willian.cardoso@icloud.com>
2017-08-08 20:27:36 -0300

    Add files via upload

--- CampoDeTEste.java
+++ CampoDeTEste.java
@@ -3,10 +3,10 @@ import java.util.Random;
 
 public class CampoDeTeste{
 
-	private int[][] matriz;
-	private int linhas;
-	private int colunas;
-	private int q;
+	protected int[][] matriz;
+	protected int linhas;
+	protected int colunas;
+	protected int q;
 	
 
 	public CampoDeTeste(int m, int n, int q) {
@@ -22,27 +22,37 @@ public class CampoDeTeste{
 		}
 		//até aqui o construtor gera a matriz zeros do tamanho que quizermos (m linhas, n colunas)
 		//a próxima linha chama o método para adicionar as bombas
-		this.preencheBombas(q);
+		this.preencheBombas(m,n,q);
+		this.preencheQuantasBombasNaVizinhanca();
 	}
 	 
 
-	public void imprimeCampoDeTeste(){
+	public void imprimeCampoFechado(){
 		for(int l=0; l<linhas; l++){
 			for (int c=0; c<colunas; c++){
-				System.out.print(matriz[l][c]+",");
+				System.out.print("."+" ");
 			}
 			System.out.println();
 		}		
 	}
 
-	private void preencheBombas(int q) {
+	public void imprimeCampoDeTeste(){
+		for(int l=0; l<linhas; l++){
+			for (int c=0; c<colunas; c++){
+				System.out.print(matriz[l][c]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+	private void preencheBombas(int m, int n, int q) {
 
 		Random gerador = new Random();
 		//numero de bombas q
 		int bombas = q;
 		//numero de elementos da matriz
-		int linh = 5;
-		int colun = 5;
+		int linh = m;
+		int colun = n;
 
 		//imprime sequência de (bombas) com as posições aleatorios
 		//numero1 e numero2 a seguir são os indices, da matriz, sorteados randomicamente, onde as bombas serão alocadas
@@ -51,7 +61,7 @@ public class CampoDeTeste{
 			if (numero1 < 0){
 				numero1 *= -1;
 			}
-			while (numero1 > linh){
+			while (numero1 >= linh){
 				numero1 /=10;
 			}
 			System.out.println("numero1: "+numero1);
@@ -59,7 +69,7 @@ public class CampoDeTeste{
 			if (numero2 < 0){
 				numero2 *= -1;
 			}
-			while (numero2 > colun){
+			while (numero2 >= colun){
 				numero2 /=10;
 			}
 			System.out.println("numero2: "+numero2);
@@ -69,24 +79,62 @@ public class CampoDeTeste{
 		}
 	}
 
-	// private void preencheQuantasBombasNaVizinhanca(int m, int n){
-	// 	int bombasVizinho = 0;
-	// 	int linhas = m;
-	// 	int colunas = n;
-	// 	for (i=0;i<linhas;i++){
-	// 		for(j=0;j<colunas;j++){
-	// 			if (matriz[i][j]!=-1){
+	private void preencheQuantasBombasNaVizinhanca(){
+		int m = this.matriz.length;
+		int n = this.matriz[0].length;
+		for (int i=0;i<m;i++){
+			for(int j=0;j<n;j++){
+				if (matriz[i][j]!=-1){
+					
+					this.matriz[i][j]= this.contaBombasNaVizinhança(i,j);	
+				}
+			}
+		}
+	}
 
+	public int contaBombasNaVizinhança(int i, int j){
+		int m = this.matriz.length;
+		int n = this.matriz[0].length;
+		int bombasVizinho = 0;
+		for(int ii=i-1;ii<=i+1;ii++){
+			for(int jj=j-1;jj<=j+1;jj++){
+				if(ii>=0 && jj>=0 && ii<linhas && jj<colunas && matriz[ii][jj]==-1){
+					bombasVizinho +=1;
+				}
+			}
+		}
+		return bombasVizinho;
+	}
+
+	// public void abrePosicaoAtualEVizinhos(int i, int j){
+	// 	// Base da recursão:
+
+	// 	if a posição (i,j) está fechada{
+	// 		Abra a posição (i,j).
+	// 		if (matriz[i][j]==0){
+	// 			for() cada vizinho de (i,j)
+	// 			{
+	// 				Seja (i2,j2) o vizinho atualmente examinado.
+	// 				if a posição (i2,j2) não é uma bomba
+	// 				{
+	// 					abrePosiçãoAtualEVizinhos(i2, j2)
+	// 				}
 	// 			}
-
 	// 		}
-
 	// 	}
 	// }
 
+
 	public static void main (String[] args){
-		CampoDeTeste campo1 = new CampoDeTeste(10,10,10);
+		CampoDeTeste campo1 = new CampoDeTeste(4,4,1);
 		campo1.imprimeCampoDeTeste();
+		System.out.print("Digite a operacao (1: abrir; 2: marcar/desmarcar): ");
+		int operacao = Teclado.leInteiro();
+		System.out.print("Digite a linha (comeca de 0): ");
+		int linhaDigitada = Teclado.leInteiro();
+		System.out.print("Digite a coluna (comeca de 0): ");
+		int colunaDigitada = Teclado.leInteiro();
+
 	}
 
 
